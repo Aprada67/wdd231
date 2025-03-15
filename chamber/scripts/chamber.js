9c4e13eb93500fb723adb23baf0068f32f766ec0
Aprada67 <apradaparmera@byupathway.edu>
2025-03-15 23:02:39 +0000

    add loading lazy finish js cards finish js list/grid buttons

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,4 +1,6 @@
+// ---------------------------
 // ----- Hamburguer menu -----
+// ---------------------------
 function toggleMenu() {
     const menu = document.getElementById("menu");
     const menuIcon = document.getElementById("menu-icon");
@@ -34,7 +36,27 @@ document.querySelectorAll("#menu li a").forEach(link => {
     });
 });
 
+// ---------------------------------
+// ----- Grid and List Buttons -----
+// ---------------------------------
+const gridButton = document.querySelector("#grid");
+const listButton = document.querySelector("#list");
+const display = document.querySelector("#cards")
+
+gridButton.addEventListener("click", () => {
+    display.classList.add("company-grid");
+    display.classList.remove("company-list");
+});
+
+listButton.addEventListener("click", () => {
+    display.classList.add("company-list");
+    display.classList.remove("company-grid");
+});
+
+
+// -----------------
 // ----- Cards -----
+// -----------------
 const url = "data/members.json";
 const cards = document.querySelector("#cards");
 
@@ -42,10 +64,48 @@ const cards = document.querySelector("#cards");
 async function getCompaniesData() {
     const response = await fetch(url);
     const data = await response.json();
-    console.table(data.companies)
-    //displayCompanies();
+    displayCompanies(data.companies);
 }
 
-getCompaniesData();
+getCompaniesData(url);
 
 // Build company card
+const displayCompanies = (companies) => {
+    companies.forEach((company) => {
+        // Create elements to add to the div.cards element
+        let card = document.createElement("div");
+        let businessName = document.createElement("h3");
+        let sector = document.createElement("p")
+        let logo = document.createElement("img");
+        let address = document.createElement("p");
+        let phone = document.createElement("p");
+        let url = document.createElement("a");
+
+        // Set classes
+        card.classList.add("company-card");
+
+        // Set information
+        businessName.textContent = company.name;
+        sector.textContent = company.sector;
+        address.textContent = company.address;
+        phone.textContent = company.phoneNumber;
+        url.textContent = company.url;
+
+        // Set image attributes
+        logo.setAttribute("src", company.image);
+        logo.setAttribute("alt", `${company.name} Logo`);
+        logo.setAttribute("loading", "lazy");
+        logo.setAttribute('width', '200');
+        logo.setAttribute('height', '200');
+
+        // Append created elements to card div
+        card.appendChild(businessName);
+        card.appendChild(sector);
+        card.appendChild(logo);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(url);
+
+        cards.appendChild(card);
+    });
+}
