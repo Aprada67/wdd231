7028d3b05d25c7ac850aeeff9fb4df445c7614e6
Alberto Prada <albertojprada10@gmail.com>
2025-04-06 18:38:26 +0100

    init

---
+++ website-project/scripts/getdate.js
@@ -0,0 +1,10 @@
+// Get actual year
+const currentYear = new Date().getFullYear();
+document.getElementById("copyrigth").textContent = `© ${currentYear} Nutrisport Lisbon`;
+
+// Last modification
+const lastModifiedDate = new Date(document.lastModified);
+const options = {year: "numeric", month: "2-digit", day: "2-digit", hour: "2-digit", minute: "2-digit", second: "2-digit", hour12: false};
+const formattedDate = lastModifiedDate.toLocaleString("en-US", options);
+
+document.getElementById("last-modified").textContent = `Last Modification: ${formattedDate}`;
